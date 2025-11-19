eb82023fb2bd9cbed8c256db9d4afabb047ab7b9
IslamMohamedAhmed <islammohamedahmed49@gmail.com>
2025-11-19 17:09:41 +0200

    fixing an error

--- src/Services/email/resend.js
+++ src/Services/email/resend.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 import { createTemplate } from './emailTemplate.js';
-
+import jwt from 'jsonwebtoken';
 export const resendEmail = async (email) => {
     let token = jwt.sign({ email }, process.env.JWT_KEY_SIGNUP);
     const resend = new Resend(process.env.RESEND_API_KEY);
